318676330bffc7f343e2aadcf95b616c6229952d
DThang200 <thangdao202@gmail.com>
2024-10-14 02:52:17 +0700

    update

---
+++ asset/test.bat
@@ -0,0 +1,3 @@
+@echo
+echo 123
+pause
--- relunch.bat
+++ relunch.bat
@@ -2,3 +2,5 @@
 cd "%USERPROFILE%\Desktop\dow-code\test-something\asset"
 xcopy "text1.txt" "%USERPROFILE%\Desktop\" /E /I /Y
 xcopy "text2.txt" "%USERPROFILE%\Desktop\" /E /I /Y
+
+start text.bat
