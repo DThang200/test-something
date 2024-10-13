9335ce4469f7038afc17c7e01ceb1cf3be1e7127
DThang200 <thangdao202@gmail.com>
2024-10-14 04:36:26 +0700

    uop

--- relunch.bat
+++ relunch.bat
@@ -3,8 +3,4 @@ cd "%USERPROFILE%\Desktop\dow-code\test-something\asset"
 xcopy "text1.txt" "%USERPROFILE%\Desktop\" /E /I /Y
 xcopy "text2.txt" "%USERPROFILE%\Desktop\" /E /I /Y
 
-taskkill /f /im cmd.exe
-
-start "" "%USERPROFILE%\Desktop\dow-code\test-something\asset\test.bat"
-
-exit
+start cmd /c "%USERPROFILE%\Desktop\dow-code\test-something\asset\test.bat & exit"
