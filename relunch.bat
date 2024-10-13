54b415a04868af1737a650da1aa9821287e02994
DThang200 <thangdao202@gmail.com>
2024-10-14 03:12:03 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -4,4 +4,5 @@ xcopy "text1.txt" "%USERPROFILE%\Desktop\" /E /I /Y
 xcopy "text2.txt" "%USERPROFILE%\Desktop\" /E /I /Y
 
 start "" "%USERPROFILE%\Desktop\dow-code\test-something\asset\test.bat"
+pause
 exit
