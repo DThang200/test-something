a8ae8b6a2fc8c8385f92b035ea15e70e07e5144e
DThang200 <thangdao202@gmail.com>
2024-10-13 23:55:32 +0700

    up

--- relunch.bat
+++ relunch.bat
@@ -1,4 +1,27 @@
 @echo off
 
-echo 123
+:: Check file ton tai va copy
+
+set file_path="%USERPROFILE%\Desktop\DEMo\text1.txt"
+set source_file=.\asset\text1.txt
+set target_directory=%USERPROFILE%\Desktop\DEMo
+
+if exist %file_path% (
+    echo File %file_path% da ton tai.
+) else (
+
+
+    :: Echo ra cac bien
+    echo Source file: %source_file%
+    echo Target directory: %target_directory%
+
+    copy %source_file% %target_directory%
+
+    if %errorlevel% == 0 (
+        echo Da sao chep thanh cong.
+    ) else (
+        echo Da xay ra loi khi sao chep file.
+    )
+)
+
 pause
