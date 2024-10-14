c05c717d503fc801544234c7b80503fed8d31073
DThang200 <thangdao202@gmail.com>
2024-10-14 23:42:06 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,10 +1,10 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-14/10/2024-11:41" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-14/10/2024-11:42" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-14/10/2024-11:41" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-14/10/2024-11:42" 
     copy /y "C:\Users\ASUS\Desktop\dow-code\test-something\asset\%" "%USERPROFILE%\Desktop" 
     start "" "%USERPROFILE%\Desktop\custom-code.bat" 
 ) 
