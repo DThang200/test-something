840c5e937e8d0aa76840b47f85b69a4fb3e11b8c
DThang200 <thangdao202@gmail.com>
2024-10-15 00:23:39 +0700

    update

--- asset/custom-code.bat
+++ asset/custom-code.bat
@@ -1,5 +1,20 @@
+timeout /t 20 
 @echo off
-echo Da update file thanh cong
-pause
-timeout /t 5
-exit
+cd /d "C:\LDPlayer\LDPlayer9\"
+ldconsole.exe quitall
+timeout /t 10
+@echo off
+for /l %%i in (1,1,50) do (
+
+	ldconsole.exe remove --index %%i
+	echo Iteration %%i
+	
+   )
+timeout /t 10
+
+cd "C:\LDPlayer\LDPlayer9\"
+
+   ldconsole.exe  restore --index 0 --file "%USERPROFILE%\Downloads\delta-Test.ldbk"
+echo Da backup
+timeout /t 20
+shutdown /r /t 0
