926e47887e0ccc8fd0045ffe59c1f6f84061c459
DThang200 <thangdao202@gmail.com>
2024-10-14 20:03:43 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,8 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "REBOOT-14/10/2024-07:59" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "REBOOT-14/10/2024-08:03" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
     call "%USERPROFILE%\Desktop\B1-tatLD.bat" 
-    shutdown /r /f /t 0 
 ) 
