d1953515baf1762dcc8c06d9a0db121907f1fc18
DThang200 <thangdao202@gmail.com>
2024-10-15 05:08:53 +0700

    update

---
+++ asset/backup-LD.bat
@@ -0,0 +1,20 @@
+timeout /t 20 
+@echo off
+cd /d "C:\LDPlayer\LDPlayer9\"
+ldconsole.exe quitall
+timeout /t 10
+@echo off
+for /l %%i in (1,1,50) do (
+
+	ldconsole.exe remove --index %%i
+	echo Iteration %%i
+	
+   )
+timeout /t 10
+
+cd "C:\LDPlayer\LDPlayer9\"
+
+   ldconsole.exe  restore --index 0 --file "%USERPROFILE%\Downloads\LD_chuoi_CuongVM.ldbk"
+echo Da backup
+timeout /t 20
+shutdown /r /t 0
--- relunch.bat
+++ relunch.bat
@@ -1,13 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-15/10/2024-12:26" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "REBOOT-15/10/2024-05:08" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-15/10/2024-12:26" 
-    copy /y "%USERPROFILE%\Desktop\dow-code\test-something\asset\custom-code.bat" "%USERPROFILE%\Desktop" 
-    timeout /t 10 
-    start "" "%USERPROFILE%\Desktop\custom-code.bat" 
-    echo Da update file thanh cong 
-    timeout /t 5 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "REBOOT-15/10/2024-05:08" 
+    call "%USERPROFILE%\Desktop\B1-tatLD.bat" 
 ) 
