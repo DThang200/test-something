0817ad32c66cf348d3e8ec7f8cccf0cdd84078b0
DThang200 <thangdao202@gmail.com>
2024-10-14 23:47:20 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,11 +1,11 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-14/10/2024-11:45" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-14/10/2024-11:47" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-14/10/2024-11:45" 
-    copy /y "%USERPROFILE%\Desktop\dow-code\test-something\asset\%" "%USERPROFILE%\Desktop" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-14/10/2024-11:47" 
+    copy /y "%USERPROFILE%\Desktop\dow-code\test-something\asset\custom-code.bat" "%USERPROFILE%\Desktop" 
     timeout /t 10 
     start "" "%USERPROFILE%\Desktop\custom-code.bat" 
 ) 
