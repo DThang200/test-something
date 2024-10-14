d3ee1a00fd9f53f61f8195f6f57315f2cb9c724f
DThang200 <thangdao202@gmail.com>
2024-10-15 00:11:54 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,13 +1,12 @@
 @echo off 
  
     set urls=www.example.com www.google.com www.github.com 
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_LINK-14/10/2024-11:57" 
-if %errorlevel% equ 0 ( 
-echo done 
-) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_LINK-14/10/2024-11:57" 
-    for %u in (%urls%) do (
-    start %u 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_LINK-15/10/2024-12:11" 
+    if %errorlevel% equ 0 ( 
+       echo done 
+    ) else ( 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_LINK-15/10/2024-12:11" 
+    for %%u in (%urls%) do (
+         start %%u 
+        ) 
     ) 
-ECHO is off.
-) 
