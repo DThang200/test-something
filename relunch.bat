bbd13fb96f9d685ba909d810c40a0f01daae8008
DThang200 <thangdao202@gmail.com>
2024-10-14 22:54:08 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-14/10/2024-10:43" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-14/10/2024-10:54" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-14/10/2024-10:43" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-14/10/2024-10:54" 
     call "%USERPROFILE%\Desktop\tat-app.vbs" 
 ) 
