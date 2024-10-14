10c6be34ed64523d6241fc308829f2f041e1c937
DThang200 <thangdao202@gmail.com>
2024-10-14 23:57:37 +0700

    update

--- asset/custom-code.bat
+++ asset/custom-code.bat
@@ -1,3 +1,5 @@
 @echo off
 echo Da update file thanh cong
 pause
+timeout /t 5
+exit
--- relunch.bat
+++ relunch.bat
@@ -1,11 +1,13 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-14/10/2024-11:47" 
+    set urls=www.example.com www.google.com www.github.com 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_LINK-14/10/2024-11:57" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-14/10/2024-11:47" 
-    copy /y "%USERPROFILE%\Desktop\dow-code\test-something\asset\custom-code.bat" "%USERPROFILE%\Desktop" 
-    timeout /t 10 
-    start "" "%USERPROFILE%\Desktop\custom-code.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_LINK-14/10/2024-11:57" 
+    for %u in (%urls%) do (
+    start %u 
+    ) 
+ECHO is off.
 ) 
