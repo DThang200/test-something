ae1dc3c3603c5be22262697e82ce713b17c9cd9b
DThang200 <thangdao202@gmail.com>
2024-10-14 15:27:59 +0700

    update

--- relunch.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-::shutdown /r /t 0
-cd "%USERPROFILE%\Desktop\dow-code\test-something\asset"
-xcopy "text1.txt" "%USERPROFILE%\Desktop\" /E /I /Y
-xcopy "text2.txt" "%USERPROFILE%\Desktop\" /E /I /Y
-
-start cmd /c "%USERPROFILE%\Desktop\dow-code\test-something\asset\test.bat & exit"
