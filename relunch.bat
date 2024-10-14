e6360af88b211183d241ae732b62713517ea3b58
DThang200 <thangdao202@gmail.com>
2024-10-14 23:42:44 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -6,5 +6,6 @@ echo done
 ) else ( 
 call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-14/10/2024-11:42" 
     copy /y "C:\Users\ASUS\Desktop\dow-code\test-something\asset\%" "%USERPROFILE%\Desktop" 
+    timeout /t 10 
     start "" "%USERPROFILE%\Desktop\custom-code.bat" 
 ) 
