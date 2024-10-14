b11309373c3f6bd96b9cd174feaa72d1a65b08db
DThang200 <thangdao202@gmail.com>
2024-10-15 00:15:26 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,13 +1,12 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-15/10/2024-12:13" 
-if %errorlevel% equ 0 ( 
-echo done 
-) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-15/10/2024-12:13" 
-    copy /y "%USERPROFILE%\Desktop\dow-code\test-something\asset\custom-code.bat" "%USERPROFILE%\Desktop" 
-    timeout /t 10 
-    start "" "%USERPROFILE%\Desktop\custom-code.bat" 
-    echo Da update file thanh cong 
-    timeout /t 5 
-) 
+    set urls=https://download1478.mediafire.com/tz2fm69g33yg2u43Taov6sv9rV-20n8l4debRiUPjCTG3X7egmSS5PfgSKPgNmLrBLWT9bY08BTXTIJeZDXB5MOdVU8dTd6puYerroaSmnZYGKdr1mjZkcCaw0mGmHQuNZ4ASoAcrHDm8LkzFIhwvrIujRikhaJD5K_Sve9bGZ9KIA/011pkv59ylrgk7i/delta-Test.ldbk 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_LINK-15/10/2024-12:15" 
+    if %errorlevel% equ 0 ( 
+       echo done 
+    ) else ( 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_LINK-15/10/2024-12:15" 
+    for %%u in (%urls%) do (
+         start %%u 
+        ) 
+    ) 
