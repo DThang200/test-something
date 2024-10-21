333f041355fe7451c8828b2012ebcce6db65ec50
DThang200 <thangdao202@gmail.com>
2024-10-21 21:29:10 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
-@echo off
-
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TESTCONFLCT-21/10/2024"
-if %errorlevel% equ 0 (
-echo "Da op thanh cong"
-) else (
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TESTCONFLCT-21/10/2024"
-echo "1243asdfas23452345 "
-)
+@echo off 
+ 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TESTTTTTTTTTTTTT-21/10/2024-09:29" 
+if %errorlevel% equ 0 ( 
+echo done 
+) else ( 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TESTTTTTTTTTTTTT-21/10/2024-09:29" 
+    echo "123" 
+) 
