af7d9393cf33d45bf77137804994d4c463683856
DThang200 <89034734+DThang200@users.noreply.github.com>
2024-10-21 21:06:35 +0700

    Update relunch.bat
    TESTCONFLCT-21/10/2024

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
-@echo off 
- 
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "REBOOT-Sun/10/20-09:36" 
-if %errorlevel% equ 0 ( 
-echo done 
-) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "REBOOT-Sun/10/20-09:36" 
-    call "%USERPROFILE%\Desktop\B1-tatLD.bat" 
-) 
+@echo off
+
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TESTCONFLCT-21/10/2024"
+if %errorlevel% equ 0 (
+echo "Da op thanh cong"
+) else (
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TESTCONFLCT-21/10/2024"
+echo "12345"
+)
