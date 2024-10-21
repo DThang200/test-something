39d3f7720bd87c6535419b649a9e2c499109482a
DThang200 <89034734+DThang200@users.noreply.github.com>
2024-10-21 21:21:37 +0700

    Update relunch.bat

--- relunch.bat
+++ relunch.bat
@@ -5,5 +5,5 @@ if %errorlevel% equ 0 (
 echo "Da op thanh cong"
 ) else (
 call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TESTCONFLCT-21/10/2024"
-echo "1243"
+echo "12432311241351352365"
 )
