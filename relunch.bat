ada7c365b8f2f09e4fca252031e5b591bd275ba7
DThang200 <thangdao202@gmail.com>
2024-10-21 21:22:17 +0700

    ─É├ú giß║úi quyß║┐t xung ─æß╗Öt

--- relunch.bat
+++ relunch.bat
@@ -5,5 +5,5 @@ if %errorlevel% equ 0 (
 echo "Da op thanh cong"
 ) else (
 call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TESTCONFLCT-21/10/2024"
-echo "1243"
+echo "1243asdfasdag à aesf "
 )
