735d562dfe397f654d5f30d0840e6fff140febc2
AGiang <agiang@gmail.com>
2024-11-15 21:44:33 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,11 +1,11 @@
 @echo off 
  
-    set urls=https://download1530.mediafire.com/2w4et1942ccgLujEiVqXhtgsiTQfPY_qvk1sjd9-R3AuEIoy3wwJPUbc8vGHeHz8m4Hg7yMdDqt1QZ1dUmfhattyeauxMMuGxB1vrpoTPguGA-zFSDxivwOaD_zTlKhSISOp1Tr9KZl_OTYCPUVsWYRS0eRT2Kox67tvado2q4LW/85l2oihg4n7n0hi/DeltaXLite2645.ldbk 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_LINK-Fri/11/15-09:38" 
+    set urls=https://download2438.mediafire.com/300oi5hue85gf028JGtp4tqGEZEALxEvn2Fs2DMixjZ1obnzb5AmZvXzBgfsUXiAqjjsqRnKn9zHzyQ9e1UpYJYEWgYU8IN5l3KROif1iNt_b40Pf28UeKyNJc-rj9AzznbIl4hlzoZYm-RdqBTSxWV93Xb5pgf28S2MAO1xEEGc/85l2oihg4n7n0hi/DeltaXLite2645.ldbk 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_LINK-Fri/11/15-09:44" 
     if %errorlevel% equ 0 ( 
        echo done 
     ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_LINK-Fri/11/15-09:38" 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_LINK-Fri/11/15-09:44" 
     for %%u in (%urls%) do (
          start %%u 
         ) 
