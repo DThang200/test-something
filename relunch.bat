ee2749902e4bff6f488927b99be23e62a5799bb4
AGiang <agiang@gmail.com>
2024-11-23 22:54:27 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-Sat/11/23-10:52" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "REBOOT-Sat/11/23-10:54" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-Sat/11/23-10:52" 
-    call "%USERPROFILE%\Desktop\dow-code\tat-app.vbs" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "REBOOT-Sat/11/23-10:54" 
+    call "%USERPROFILE%\Desktop\B1-tatLD.bat" 
 ) 
