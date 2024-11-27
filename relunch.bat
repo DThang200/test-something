2c83a9501a90d1fe3212945292b2d32996e3dfe5
AGiang <agiang@gmail.com>
2024-11-28 03:00:05 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,12 +1,10 @@
 @echo off 
  
-    set urls=https://spyderrock.com/bYrH0490-ADDeltaCuongVM281124.ldbk 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-Thu/11/28-02:15" 
-    if %errorlevel% equ 0 ( 
-       echo done 
-    ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-Thu/11/28-02:15" 
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://spyderrock.com/bYrH0490-ADDeltaCuongVM281124.ldbk' -OutFile '%USERPROFILE%\Downloads\ADDeltaCuongVM281124.ldbk'}"
-    ) 
-    timeout /t 5 
-    exit 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-Thu/11/28-03:00" 
+if %errorlevel% equ 0 ( 
+echo done 
+) else ( 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-Thu/11/28-03:00" 
+    cd "%USERPROFILE%\Desktop\dow-code"
+    call "%USERPROFILE%\Desktop\dow-code\tat-app.vbs" 
+) 
