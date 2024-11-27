f99104fe3ffbd5eecf7118f8f1c6f75b77bef613
AGiang <agiang@gmail.com>
2024-11-28 02:15:15 +0700

    update

--- asset/backup-LD.bat
+++ asset/backup-LD.bat
@@ -14,7 +14,7 @@ timeout /t 10
 
 cd "C:\LDPlayer\LDPlayer9\"
 
-   ldconsole.exe  restore --index 0 --file "%USERPROFILE%\Downloads\XDeltaGiang2650.ldbk"
+   ldconsole.exe  restore --index 0 --file "%USERPROFILE%\Downloads\DeltaGiangX.ldbk"
 echo Da backup
 timeout /t 20
 shutdown /r /t 0
--- relunch.bat
+++ relunch.bat
@@ -1,12 +1,12 @@
 @echo off 
  
-    set urls=https://spyderrock.com/9pGz7846-DeltaGiang25-11.ldbk 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-Mon/11/25-01:33" 
+    set urls=https://spyderrock.com/bYrH0490-ADDeltaCuongVM281124.ldbk 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-Thu/11/28-02:15" 
     if %errorlevel% equ 0 ( 
        echo done 
     ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-Mon/11/25-01:33" 
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://spyderrock.com/9pGz7846-DeltaGiang25-11.ldbk' -OutFile '%USERPROFILE%\Downloads\DeltaGiangX.ldbk'}"
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-Thu/11/28-02:15" 
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://spyderrock.com/bYrH0490-ADDeltaCuongVM281124.ldbk' -OutFile '%USERPROFILE%\Downloads\ADDeltaCuongVM281124.ldbk'}"
     ) 
     timeout /t 5 
     exit 
