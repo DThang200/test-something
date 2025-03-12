1e7c1833a05bef01674ed39383d9084a33fef6f1
DThang200 <thangdao202@gmail.com>
2025-03-12 11:01:21 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,11 +1,11 @@
 @echo off 
  
     set urls=https://spyderrock.com/67gx7799-CodeXv2662.ldbk 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-12/03/2025-11:00" 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-12/03/2025-11:01" 
     if %errorlevel% equ 0 ( 
        echo done 
     ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-12/03/2025-11:00" 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-12/03/2025-11:01" 
     powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://spyderrock.com/67gx7799-CodeXv2662.ldbk' -OutFile '%USERPROFILE%\Downloads\CodeXv2662.ldbk'}" 
     timeout /t 5 
     exit 
