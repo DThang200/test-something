0529bb72d1071728442fc7f9d2ff313c3716d3a6
DThang200 <thangdao202@gmail.com>
2025-03-12 13:53:42 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,10 +1,12 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-12/03/2025-12:51" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-12/03/2025-13:53" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-12/03/2025-12:51" 
-    cd "%USERPROFILE%\Desktop\dow-code" 
-    call "%USERPROFILE%\Desktop\dow-code\tat-app.vbs" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-12/03/2025-13:53" 
+    copy /y "%USERPROFILE%\Desktop\dow-code\test-something\asset\backup-LD.bat" "%USERPROFILE%\Desktop\dow-code" 
+    timeout /t 10 
+    echo Da update file thanh cong 
+    timeout /t 5 
 ) 
