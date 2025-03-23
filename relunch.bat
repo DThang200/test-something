593d943fb0ebe6a57f39dc9f85ae578fcb6f9bff
DThang200 <thangdao202@gmail.com>
2025-03-24 01:16:56 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-23/03/2025-21:31" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-24/03/2025-01:16" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-23/03/2025-21:31" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-24/03/2025-01:16" 
     shutdown /r /f /t 15 
 ) 
