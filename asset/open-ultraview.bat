36ca21855f0d1a6571ef36768c1413bdadf115af
DThang200 <thangdao202@gmail.com>
2025-04-02 02:38:25 +0700

    update

---
+++ asset/open-ultraview.bat
@@ -0,0 +1,7 @@
+@echo off
+:: Mở UltraViewer từ thư mục C:\Program Files (x86)\UltraViewer
+start "" "C:\Program Files (x86)\UltraViewer\UltraViewer.exe"
+
+:: Thông báo đã mở UltraViewer thành công
+echo Đã mở UltraViewer thành công!
+exit
--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CAPTURE-02/04/2025-02:24" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKFREESPACE-02/04/2025-02:38" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CAPTURE-02/04/2025-02:24" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\capture.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKFREESPACE-02/04/2025-02:38" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
 ) 
