17d3a0bf6c3f899607b7c9996fad43e2f513bc2a
DThang200 <thangdao202@gmail.com>
2025-04-02 02:45:45 +0700

    update

--- asset/open-ultraview.bat
+++ asset/open-ultraview.bat
@@ -1,6 +1,13 @@
 @echo off
 :: Mở UltraViewer từ thư mục C:\Program Files (x86)\UltraViewer
-start "" "C:\Program Files (x86)\UltraViewer\UltraViewer.exe"
+start "" "C:\Program Files (x86)\UltraViewer\UltraViewer_Desktop.exe"
+
+timeout /t 10 >nul
+
+cd %USERPROFILE%\Desktop\dow-code\test-something\asset
+
+nircmd.exe win -top ititle "UltraViewer"
+
 
 :: Thông báo đã mở UltraViewer thành công
 echo Đã mở UltraViewer thành công!
--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKFREESPACE-02/04/2025-02:38" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKING-02/04/2025-02:45" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKFREESPACE-02/04/2025-02:38" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKING-02/04/2025-02:45" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\open-ultraview.bat" 
 ) 
