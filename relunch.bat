7e659823648e57339d34abad89c0399160b95633
DThang200 <thangdao202@gmail.com>
2025-04-03 21:02:16 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "REBOOT-03/04/2025-19:55" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKING-03/04/2025-21:02" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "REBOOT-03/04/2025-19:55" 
-    call "%USERPROFILE%\Desktop\B1-tatLD.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKING-03/04/2025-21:02" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\open-ultraview.bat" 
 ) 
