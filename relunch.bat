a43b379b14f04016bae47c4b5e137292191a1d96
DThang200 <thangdao202@gmail.com>
2025-04-06 15:04:05 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CAPTURE-05/04/2025-17:26" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-06/04/2025-15:04" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CAPTURE-05/04/2025-17:26" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\capture.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-06/04/2025-15:04" 
+    shutdown /r /f /t 15 
 ) 
