21f09a00a6b2a18b72e301cf61e0348de6672a67
DThang200 <thangdao202@gmail.com>
2025-04-11 23:27:14 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,10 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-11/04/2025-22:28" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-11/04/2025-23:27" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-11/04/2025-22:28" 
-    cd "%USERPROFILE%\Desktop\dow-code\test-something\asset" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\tat-app.vbs" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-11/04/2025-23:27" 
+    shutdown /r /f /t 15 
 ) 
