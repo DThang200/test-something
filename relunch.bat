4601029571e63d71746544f338931ada928209e2
DThang200 <thangdao202@gmail.com>
2025-04-12 18:14:20 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-11/04/2025-23:27" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CAPTURE-12/04/2025-18:14" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-11/04/2025-23:27" 
-    shutdown /r /f /t 15 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CAPTURE-12/04/2025-18:14" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\capture.bat" 
 ) 
