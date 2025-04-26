652f7c91868eb762e50799647c51c154b063cacc
DThang200 <thangdao202@gmail.com>
2025-04-26 14:48:10 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "REBOOT-25/04/2025-18:31" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "REBOOT-26/04/2025-14:48" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "REBOOT-25/04/2025-18:31" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "REBOOT-26/04/2025-14:48" 
     call "%USERPROFILE%\Desktop\B1-tatLD.bat" 
 ) 
