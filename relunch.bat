b3411457de19a07f67478fc6effd4d258ea22a31
DThang200 <thangdao202@gmail.com>
2025-04-29 00:55:19 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKING-29/04/2025-00:49" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKFREESPACE-29/04/2025-00:55" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKING-29/04/2025-00:49" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\open-ultraview.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKFREESPACE-29/04/2025-00:55" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
 ) 
