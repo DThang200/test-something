dc927a6a187289fa7dc8a1a4114326745f0ae848
DThang200 <thangdao202@gmail.com>
2025-06-01 03:51:34 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "SHUTDOWN-SELECTED-01/06/2025-03:42" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "SHUTDOWN-SELECTED-01/06/2025-03:51" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "SHUTDOWN-SELECTED-01/06/2025-03:42" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "SHUTDOWN-SELECTED-01/06/2025-03:51" 
     call "%USERPROFILE%\Desktop\dow-code\test-something\asset\shutdown-selected.bat" 
 ) 
