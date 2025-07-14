49a946c8a4a63ec270aa563c63eb5689418c4b8b
DThang200 <thangdao202@gmail.com>
2025-07-14 20:14:50 +0700

    up

--- relunch.bat
+++ relunch.bat
@@ -8,8 +8,6 @@ if %errorlevel% equ 0 (
     call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-14/07/2025-19:24" 
     powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st1.ranoz.gg/XFC3HU8L-Delta2679(1).rar' -OutFile '%USERPROFILE%\Downloads\Delta2679(1).rar'}" 
     timeout /t 60 
-    cd "%USERPROFILE%\Desktop\dow-code\test-something\asset" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\after-backup.bat" 
     ) 
     timeout /t 5 
     exit 
