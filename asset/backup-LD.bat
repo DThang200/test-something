0b59b52c0ed77ac7be002f69c8f4853100fc04ac
DThang200 <thangdao202@gmail.com>
2025-07-18 04:31:25 +0700

    update

--- asset/backup-LD.bat
+++ asset/backup-LD.bat
@@ -14,7 +14,7 @@ timeout /t 10
 
 cd "C:\LDPlayer\LDPlayer9\"
 
-   ldconsole.exe  restore --index 0 --file "%USERPROFILE%\Downloads\Codexv2675.ldbk"
+   ldconsole.exe  restore --index 0 --file "%USERPROFILE%\Downloads\Delta2679_2.ldbk"
 echo Da backup
 timeout /t 20
 shutdown /r /t 0
--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,14 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-18/07/2025-03:27" 
+set urls=https://st1.ranoz.gg/0S2rI40W-Delta2679_2.rar 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-18/07/2025-04:31" 
 if %errorlevel% equ 0 ( 
-echo done 
+   echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-18/07/2025-03:27" 
-    shutdown /r /f /t 15 
-) 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-18/07/2025-04:31" 
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st1.ranoz.gg/0S2rI40W-Delta2679_2.rar' -OutFile '%USERPROFILE%\Downloads\Delta2679_2.ldbk'}" 
+    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\Delta2679_2.rar" "%USERPROFILE%\Downloads\" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\after-backup.bat" 
+    ) 
+    timeout /t 5 
+    exit 
