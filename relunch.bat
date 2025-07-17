3a8e61f60caccf9e6079ab461a2b55d856b5d5a0
DThang200 <thangdao202@gmail.com>
2025-07-18 03:27:12 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,14 +1,9 @@
 @echo off 
  
-set urls=https://st1.ranoz.gg/jNnBFiI7-Delta2679_2.rar 
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-18/07/2025-03:16" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-18/07/2025-03:27" 
 if %errorlevel% equ 0 ( 
-   echo done 
+echo done 
 ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-18/07/2025-03:16" 
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st1.ranoz.gg/jNnBFiI7-Delta2679_2.rar' -OutFile '%USERPROFILE%\Downloads\Delta2679_2.ldbk'}" 
-    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ '%USERPROFILE%\Downloads\Delta2679_2.rar' "%USERPROFILE%\Downloads\" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\after-backup.bat" 
-    ) 
-    timeout /t 5 
-    exit 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-18/07/2025-03:27" 
+    shutdown /r /f /t 15 
+) 
