2c9bdd7b259dfaaa81910f75b5e9b145b7be40ba
DThang200 <thangdao202@gmail.com>
2025-07-26 23:09:24 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CAPTURE-26/07/2025-15:32" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKFREESPACE-26/07/2025-23:09" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CAPTURE-26/07/2025-15:32" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\capture.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKFREESPACE-26/07/2025-23:09" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
 ) 
