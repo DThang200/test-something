41ab8a07714aa896288fef781d9e0eec50b71202
DThang200 <thangdao202@gmail.com>
2025-08-07 17:11:55 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,10 +1,14 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-05/08/2025-19:19" 
+set urls=https://st1.ranoz.gg/8R9ZM6SH-Delta2684.rar 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-07/08/2025-17:11" 
 if %errorlevel% equ 0 ( 
-echo done 
+   echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-05/08/2025-19:19" 
-    cd "%USERPROFILE%\Desktop\dow-code\test-something\asset" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\tat-app.vbs" 
-) 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-07/08/2025-17:11" 
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st1.ranoz.gg/8R9ZM6SH-Delta2684.rar' -OutFile '%USERPROFILE%\Downloads\Delta2684.rar'}" 
+    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\Delta2684.rar" "%USERPROFILE%\Downloads\" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\after-backup.bat" 
+    ) 
+    timeout /t 5 
+    exit 
