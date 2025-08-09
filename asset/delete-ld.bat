7b7ead7409416df66ee0690734b0acb9e5fc3fe1
DThang200 <thangdao202@gmail.com>
2025-08-09 07:13:26 +0700

    update

---
+++ asset/delete-ld.bat
@@ -0,0 +1,18 @@
+timeout /t 3
+@echo off;
+
+cd "C:\LDPlayer\LDPlayer9\"
+set LDCONSOLE="C:\LDPlayer\LDPlayer9\ldconsole.exe"
+%LDCONSOLE% quitall
+timeout /t 5 >nul
+
+timeout /t 5
+@echo off
+for /l %%i in (1,1,50) do (
+
+	ldconsole.exe remove --index %%i
+	echo Iteration %%i
+	
+   )
+timeout /t 20
+shutdown /r /t 0
--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKFREESPACE-09/08/2025-00:23" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "Delete-09/08/2025-07:13" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKFREESPACE-09/08/2025-00:23" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "Delete-09/08/2025-07:13" 
+    call "%USERPROFILE%\Desktop\delete-ld.bat" 
 ) 
