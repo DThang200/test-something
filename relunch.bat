3862bebcc16a89096dc095c4e0a1a7bc2e76d529
DThang200 <thangdao202@gmail.com>
2025-08-28 14:42:09 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,10 +1,9 @@
 @echo off 
-ECHO is off.
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CUSTOM-27/08/2025-12:39" 
+ 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "CHECKFREESPACE-28/08/2025-14:42" 
 if %errorlevel% equ 0 ( 
-    echo done 
+echo done 
 ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CUSTOM-27/08/2025-12:39" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\set-ram.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "CHECKFREESPACE-28/08/2025-14:42" 
     call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
 ) 
