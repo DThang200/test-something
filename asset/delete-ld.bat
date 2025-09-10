bd7a86b91319cb9c203b79165f46da8d6e5f1fe1
DThang200 <thangdao202@gmail.com>
2025-09-11 04:42:31 +0700

    update

--- asset/delete-ld.bat
+++ asset/delete-ld.bat
@@ -4,15 +4,31 @@ timeout /t 3
 cd "C:\LDPlayer\LDPlayer9\"
 set LDCONSOLE="C:\LDPlayer\LDPlayer9\ldconsole.exe"
 %LDCONSOLE% quitall
-timeout /t 30 >nul
+timeout /t 3 >nul
 
-timeout /t 5
 @echo off
-for /l %%i in (1,1,50) do (
+setlocal
+set "DIR=C:\LDPlayer\LDPlayer9\vms"
 
-	ldconsole.exe remove --index %%i
-	echo Iteration %%i
-	
-   )
+if not exist "%DIR%" (
+  echo Khong tim thay thu muc: "%DIR%"
+  exit /b 1
+)
+
+rem Neu dang chay, thu kill cac tien trinh LDP de giai phong file lock (khong bat buoc)
+for %%P in (LDPlayer9.exe LdVBoxHeadless.exe LdBoxSVC.exe LdVBoxSVC.exe) do (
+  taskkill /f /im %%P >nul 2>&1
+)
+
+rem Xoa tat ca folder leidian* tru leidian0
+for /d %%D in ("%DIR%\leidian*") do (
+  if /I not "%%~nD"=="leidian0" (
+    echo Deleting folder: "%%~fD"
+    rd /s /q "%%~fD" 2>nul
+  )
+)
+
+echo Done.
+
 timeout /t 20
 shutdown /r /t 0
--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-10/09/2025-23:25" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "Delete-11/09/2025-04:42" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-10/09/2025-23:25" 
-    shutdown /r /f /t 15 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "Delete-11/09/2025-04:42" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\delete-ld.bat" 
 ) 
