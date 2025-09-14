72b42b01a0b69c8a79f1f3e5daf2c59b66e1db50
DThang200 <thangdao202@gmail.com>
2025-09-14 22:30:30 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "Delete-11/09/2025-04:42" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "Delete-14/09/2025-22:30" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "Delete-11/09/2025-04:42" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "Delete-14/09/2025-22:30" 
     call "%USERPROFILE%\Desktop\dow-code\test-something\asset\delete-ld.bat" 
 ) 
