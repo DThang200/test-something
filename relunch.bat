b27fd3c9fdde8f33e6f11771968cd751b2045ff0
DThang200 <thangdao202@gmail.com>
2025-09-24 11:46:04 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,9 +1,15 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "Delete-24/09/2025-07:13" 
+set urls=https://st7.ranoz.gg/AKEQVBJn-2690vip.rar 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-24/09/2025-11:46" 
 if %errorlevel% equ 0 ( 
-echo done 
+   echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "Delete-24/09/2025-07:13" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\delete-ld.bat" 
-) 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-24/09/2025-11:46" 
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st7.ranoz.gg/AKEQVBJn-2690vip.rar' -OutFile '%USERPROFILE%\Downloads\2690vip.rar'}" 
+    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\2690vip.rar" "%USERPROFILE%\Downloads\" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\after-backup.bat" 
+    ) 
+    timeout /t 5 
+    exit 
