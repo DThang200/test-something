46e9caf4a8ff0d69d80136a69fec0f0042f4878d
DThang200 <thangdao202@gmail.com>
2025-09-24 12:21:13 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,15 +1,10 @@
 @echo off 
  
-set urls=https://st7.ranoz.gg/AKEQVBJn-2690vip.rar 
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-24/09/2025-11:46" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-24/09/2025-12:21" 
 if %errorlevel% equ 0 ( 
-   echo done 
+echo done 
 ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-24/09/2025-11:46" 
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st7.ranoz.gg/AKEQVBJn-2690vip.rar' -OutFile '%USERPROFILE%\Downloads\2690vip.rar'}" 
-    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\2690vip.rar" "%USERPROFILE%\Downloads\" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\after-backup.bat" 
-    ) 
-    timeout /t 5 
-    exit 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-24/09/2025-12:21" 
+    cd "%USERPROFILE%\Desktop\dow-code\test-something\asset" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\tat-app.vbs" 
+) 
