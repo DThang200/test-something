d9ca9f3ed5ee2c69525cf27822585cb740e35be6
DThang200 <thangdao202@gmail.com>
2025-09-29 05:38:26 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,13 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-25/09/2025-01:15" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "Delete-29/09/2025-05:38" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-25/09/2025-01:15" 
-    copy /y "%USERPROFILE%\Desktop\dow-code\test-something\asset\pull-code.bat" "%USERPROFILE%\Desktop\dow-code" 
-    timeout /t 10 
-    echo Da update file thanh cong 
-    timeout /t 5 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "Delete-29/09/2025-05:38" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\delete-ld.bat" 
 ) 
