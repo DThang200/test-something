bf86d4649bafbd7b7656f4196d4f58b5bd0a1cbb
DThang200 <thangdao202@gmail.com>
2025-09-30 15:34:59 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,15 +1,15 @@
 @echo off 
  
-set urls=https://st7.ranoz.gg/z4M2YK0f-2690vip2.rar 
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-30/09/2025-14:23" 
+set urls=https://st7.ranoz.gg/7NuXg2jg-2690vip2.rar 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-30/09/2025-15:34" 
 if %errorlevel% equ 0 ( 
    echo done 
 ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-30/09/2025-14:23" 
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st7.ranoz.gg/z4M2YK0f-2690vip2.rar' -OutFile '%USERPROFILE%\Downloads\26902vip.rar'}" 
-    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\26902vip.rar" "%USERPROFILE%\Downloads\" 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-30/09/2025-15:34" 
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st7.ranoz.gg/7NuXg2jg-2690vip2.rar' -OutFile '%USERPROFILE%\Downloads\2690vip2.rar'}" 
+    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\2690vip2.rar" "%USERPROFILE%\Downloads\" 
     call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\after-backup.bat" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\backup-LD.bat.bat" 
     ) 
     timeout /t 5 
     exit 
