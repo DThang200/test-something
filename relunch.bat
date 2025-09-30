9c6b34a2f717eeb55aa724982ec5ddd3dc09a6ff
DThang200 <thangdao202@gmail.com>
2025-09-30 16:12:10 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,14 +1,9 @@
 @echo off 
  
-set urls=https://st7.ranoz.gg/r0N8adSa-2690vip3.rar 
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-30/09/2025-15:45" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-30/09/2025-16:12" 
 if %errorlevel% equ 0 ( 
-   echo done 
+echo done 
 ) else ( 
-    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-30/09/2025-15:45" 
-    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st7.ranoz.gg/r0N8adSa-2690vip3.rar' -OutFile '%USERPROFILE%\Downloads\2690vip3.rar'}" 
-    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\2690vip3.rar" "%USERPROFILE%\Downloads\" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\backup-LD.bat.bat" 
-    ) 
-    timeout /t 5 
-    exit 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-30/09/2025-16:12" 
+    shutdown /r /f /t 15 
+) 
