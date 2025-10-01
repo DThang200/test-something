aaf9108166a6601eacf843876c1b1c84899087d0
DThang200 <thangdao202@gmail.com>
2025-10-01 08:13:34 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,10 +1,9 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "BACKUP-30/09/2025-16:51" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "RESET-01/10/2025-08:13" 
 if %errorlevel% equ 0 ( 
 echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "BACKUP-30/09/2025-16:51" 
-    cd "%USERPROFILE%\Desktop\dow-code\test-something\asset" 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\backup-LD.bat" 
+call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "RESET-01/10/2025-08:13" 
+    shutdown /r /f /t 15 
 ) 
