53bd8c363c026ab06c5158207c851f4138f529f1
DThang200 <thangdao202@gmail.com>
2025-10-06 13:38:03 +0700

    update

--- relunch.bat
+++ relunch.bat
@@ -1,13 +1,14 @@
 @echo off 
  
-call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "DOWLOAD_FILE-02/10/2025-08:47" 
+set urls=https://st7.ranoz.gg/QdoKtLb6-2692vip3-av.rar 
+call "%USERPROFILE%\Desktop\dow-code\client-code\checklog.bat" "TAI_FILE_BACKUP-06/10/2025-13:38" 
 if %errorlevel% equ 0 ( 
-echo done 
+   echo done 
 ) else ( 
-call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "DOWLOAD_FILE-02/10/2025-08:47" 
-    copy /y "%USERPROFILE%\Desktop\dow-code\test-something\asset\pull-code.bat" "%USERPROFILE%\Desktop\dow-code" 
-    timeout /t 10 
-    echo Da update file thanh cong 
+    call "%USERPROFILE%\Desktop\dow-code\client-code\addlog.bat" "TAI_FILE_BACKUP-06/10/2025-13:38" 
+    powershell -NoProfile -ExecutionPolicy Bypass -Command "& {Invoke-WebRequest -Uri 'https://st7.ranoz.gg/QdoKtLb6-2692vip3-av.rar' -OutFile '%USERPROFILE%\Downloads\2692vip3-av.rar'}" 
+    "C:\Program Files\WinRAR\WinRAR.exe" x -ibck -o+ "%USERPROFILE%\Downloads\2692vip3-av.rar" "%USERPROFILE%\Downloads\" 
+    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\backup-LD.bat" 
+    ) 
     timeout /t 5 
-    call "%USERPROFILE%\Desktop\dow-code\test-something\asset\free-space.bat" 
-) 
+    exit 
